0924e126dbcdbb007637b23d934014474c880db2
climbingcook <58454900+climbingcook@users.noreply.github.com>
2019-12-06 22:38:29 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,4 @@
-document.getElementsByClassName('hamburger')[0]
-    .addEventListener('click', function(event) {
+document.getElementsByClassName('hamburger')[0].addEventListener('click', function(event) {
         var navArray = document.getElementsByClassName("topnav")[0].children;
         for (var i = 2; i < navArray.length; i++) {
             if (navArray[i].style.display == "none" || navArray[i].style.display == "") {
